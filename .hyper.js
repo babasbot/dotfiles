d26a9e543b4cc377d72e36a5d565500c52f92405
Luis Alfredo Lorenzo <luis@babas.bot>
2021-01-29 11:58:35 -0600

    Adds the "hyper-hide-title" plugin

--- .hyper.js
+++ .hyper.js
@@ -127,7 +127,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-font-ligatures", "hyper-monokai-pro"],
+  plugins: ["hyper-font-ligatures", "hyper-monokai-pro", "hyper-hide-title"],
   
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
